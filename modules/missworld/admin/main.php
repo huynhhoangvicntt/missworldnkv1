eba8b08a4d8fcc3e3cf836f2fb3891a15dabf0b8
Vi Huynh <hoangvicntt2k@gmail.com>
2024-07-11 21:14:06 +0700

    delete

--- modules/missworld/admin/main.php
+++ modules/missworld/admin/main.php
@@ -57,11 +57,6 @@ if ($nv_Request->get_title('delete', 'post', '') === NV_CHECK_SESSION) {
 $xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
 $xtpl->assign('LANG', $lang_module);
 $xtpl->assign('GLANG', $lang_global);
-$xtpl->assign('NV_BASE_ADMINURL', NV_BASE_ADMINURL);
-$xtpl->assign('NV_NAME_VARIABLE', NV_NAME_VARIABLE);
-$xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
-$xtpl->assign('NV_LANG_VARIABLE', NV_LANG_VARIABLE);
-$xtpl->assign('NV_LANG_DATA', NV_LANG_DATA);
 $xtpl->assign('MODULE_NAME', $module_name);
 $xtpl->assign('MODULE_FILE', $module_file);
 $xtpl->assign('DATA', $array);
