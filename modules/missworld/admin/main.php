7639cd4c44162bc0ca37af6a5396e19a04715cfb
Vi Huynh <hoangvicntt2k@gmail.com>
2024-07-11 21:10:39 +0700

    move function delete

--- modules/missworld/admin/main.php
+++ modules/missworld/admin/main.php
@@ -17,50 +17,12 @@ $page_title = $lang_module['player_manager'];
 $array = [];
 
 // Gọi CSDL để lấy dữ liệu
-$query = $db->query('SELECT * FROM ' . NV_PREFIXLANG .'_missworld_players ORDER BY weight ASC');
+$query = $db->query("SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data . "_players ORDER BY weight ASC");
 while ($row = $query->fetch()) {
     $array[$row['id']] = $row;
-   
 }
 
-$xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
-$xtpl->assign('LANG', $lang_module);
-$xtpl->assign('GLANG', $lang_global);
-$xtpl->assign('NV_BASE_ADMINURL', NV_BASE_ADMINURL);
-$xtpl->assign('NV_NAME_VARIABLE', NV_NAME_VARIABLE);
-$xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
-$xtpl->assign('NV_LANG_VARIABLE', NV_LANG_VARIABLE);
-$xtpl->assign('NV_LANG_DATA', NV_LANG_DATA);
-$xtpl->assign('MODULE_NAME', $module_name);
-$xtpl->assign('MODULE_FILE', $module_file);
-$xtpl->assign('DATA', $array);
-$xtpl->assign('OP', $op);
-
-$xtpl->assign('LINK_ADD_NEW', NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=players');
-
-//hien thi dl
-$num = sizeof($array);
-if(!empty($array)){
-    foreach($array as $value){
-        $value['dob'] = nv_date('d/m/Y', $value['dob']); 
-        $value['url_edit'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=players&amp;id=' . $value['id'];
-        $value['image'] = NV_BASE_SITEURL .NV_UPLOADS_DIR . '/' . $module_upload . '/' . $value['image'];
-        
-        $value['add_time'] = nv_date('d/m/Y H:i', $value['add_time']);
-        $value['edit_time'] = $value['edit_time'] ? nv_date('d/m/Y H:i', $value['edit_time']) : '';
-        
-        for ($i = 1; $i <= $num; ++$i) {
-        $xtpl->assign('WEIGHT', [
-            'w' => $i,
-            'selected' => ($i == $value['weight']) ? ' selected="selected"' : ''
-        ]);
-
-        $xtpl->parse('main.loop.weight');
-    }
-        $xtpl->assign('DATA', $value);
-        $xtpl->parse('main.loop');
-    }
-}
+// Xóa
 if ($nv_Request->get_title('delete', 'post', '') === NV_CHECK_SESSION) {
     $id = $nv_Request->get_absint('id', 'post', 0);
 
@@ -92,6 +54,42 @@ if ($nv_Request->get_title('delete', 'post', '') === NV_CHECK_SESSION) {
     nv_htmlOutput("OK");
 }
 
+$xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
+$xtpl->assign('LANG', $lang_module);
+$xtpl->assign('GLANG', $lang_global);
+$xtpl->assign('NV_BASE_ADMINURL', NV_BASE_ADMINURL);
+$xtpl->assign('NV_NAME_VARIABLE', NV_NAME_VARIABLE);
+$xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
+$xtpl->assign('NV_LANG_VARIABLE', NV_LANG_VARIABLE);
+$xtpl->assign('NV_LANG_DATA', NV_LANG_DATA);
+$xtpl->assign('MODULE_NAME', $module_name);
+$xtpl->assign('MODULE_FILE', $module_file);
+$xtpl->assign('DATA', $array);
+$xtpl->assign('OP', $op);
+$xtpl->assign('LINK_ADD_NEW', NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=players');
+
+// Hiển thị dữ liệu
+$num = sizeof($array);
+if (!empty($array)) {
+    foreach ($array as $value) {
+        $value['dob'] = nv_date('d/m/Y', $value['dob']); 
+        $value['url_edit'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=players&amp;id=' . $value['id'];
+        $value['image'] = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_upload . '/' . $value['image'];
+        $value['add_time'] = nv_date('d/m/Y H:i', $value['add_time']);
+        $value['edit_time'] = $value['edit_time'] ? nv_date('d/m/Y H:i', $value['edit_time']) : '';
+        
+        for ($i = 1; $i <= $num; ++$i) {
+        $xtpl->assign('WEIGHT', [
+            'w' => $i,
+            'selected' => ($i == $value['weight']) ? ' selected="selected"' : ''
+        ]);
+
+        $xtpl->parse('main.loop.weight');
+    }
+        $xtpl->assign('DATA', $value);
+        $xtpl->parse('main.loop');
+    }
+}
 
 $xtpl->parse('main');
 $contents = $xtpl->text('main');
