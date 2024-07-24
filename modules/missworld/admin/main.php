e78a091dede0b1a07ee498ff9d9aa4084265fc0c
Vi Huynh <hoangvicntt2k@gmail.com>
2024-07-24 16:02:47 +0700

    Cập nhật chức năng tìm kiếm(Sắp  xong)

--- modules/missworld/admin/main.php
+++ modules/missworld/admin/main.php
@@ -16,6 +16,24 @@ if (!defined('NV_IS_FILE_ADMIN')) {
 $page_title = $lang_module['player_manager'];
 $array = [];
 
+// Search parameters
+$array_search = [];
+$array_search['q'] = $nv_Request->get_title('q', 'get', '');
+$array_search['from'] = $nv_Request->get_title('f', 'get', '');
+$array_search['to'] = $nv_Request->get_title('t', 'get', '');
+
+// Process date parameters
+if (preg_match('/^([0-9]{1,2})\-([0-9]{1,2})\-([0-9]{4})$/', $array_search['from'], $m)) {
+    $array_search['from'] = mktime(0, 0, 0, intval($m[2]), intval($m[1]), intval($m[3]));
+} else {
+    $array_search['from'] = 0;
+}
+if (preg_match('/^([0-9]{1,2})\-([0-9]{1,2})\-([0-9]{4})$/', $array_search['to'], $m)) {
+    $array_search['to'] = mktime(23, 59, 59, intval($m[2]), intval($m[1]), intval($m[3]));
+} else {
+    $array_search['to'] = 0;
+}
+
 // Xóa
 if ($nv_Request->get_title('delete', 'post', '') === NV_CHECK_SESSION) {
     $id = $nv_Request->get_absint('id', 'post', 0);
@@ -31,16 +49,16 @@ if ($nv_Request->get_title('delete', 'post', '') === NV_CHECK_SESSION) {
     $sql = "DELETE FROM " . NV_PREFIXLANG . "_" . $module_data . "_players WHERE id=" . $id;
     $db->query($sql);
 
-     // Cập nhật thứ tự
-     $sql = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_players ORDER BY weight ASC";
-     $result = $db->query($sql);
-     $weight = 0;
- 
-     while ($row = $result->fetch()) {
-         ++$weight;
-         $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_players SET weight=" . $weight . " WHERE id=" . $row['id'];
-         $db->query($sql);
-     }
+    // Cập nhật thứ tự
+    $sql = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_players ORDER BY weight ASC";
+    $result = $db->query($sql);
+    $weight = 0;
+
+    while ($row = $result->fetch()) {
+        ++$weight;
+        $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_players SET weight=" . $weight . " WHERE id=" . $row['id'];
+        $db->query($sql);
+    }
 
     nv_insert_logs(NV_LANG_DATA, $module_name, 'LOG_DELETE_PLAYER', json_encode($array), $admin_info['admin_id']);
     $nv_Cache->delMod($module_name);
@@ -54,6 +72,26 @@ $base_url = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_D
 
 // Gọi CSDL để lấy dữ liệu
 $db->sqlreset()->select('COUNT(*)')->from(NV_PREFIXLANG . "_" . $module_data . "_players");
+
+$where = [];
+if (!empty($array_search['q'])) {
+    $base_url .= '&amp;q=' . urlencode($array_search['q']);
+    $dblikekey = $db->dblikeescape($array_search['q']);
+    $where[] = "(fullname LIKE '%" . $dblikekey . "%' OR keywords LIKE '%" . $dblikekey . "%')";
+}
+if (!empty($array_search['from'])) {
+    $base_url .= '&amp;f=' . nv_date('d-m-Y', $array_search['from']);
+    $where[] = "dob>=" . $array_search['from'];
+}
+if (!empty($array_search['to'])) {
+    $base_url .= '&amp;t=' . nv_date('d-m-Y', $array_search['to']);
+    $where[] = "dob<=" . $array_search['to'];
+}
+
+if (!empty($where)) {
+    $db->where(implode(' AND ', $where));
+}
+
 $sql = $db->sql();
 $total = $db->query($sql)->fetchColumn();
 
@@ -63,45 +101,6 @@ $sql = $db->sql();
 $result = $db->query($sql);
 while ($row = $result->fetch()) {
     $array[$row['id']] = $row;
-}
-
-// Phần tìm kiếm
-$array_search = [];
-$array_search['q'] = $nv_Request->get_title('q', 'get', '');
-$array_search['from'] = $nv_Request->get_title('f', 'get', '');
-$array_search['to'] = $nv_Request->get_title('t', 'get', '');
-
-// Xử lý dữ liệu tìm kiếm
-if (preg_match('/^([0-9]{1,2})\-([0-9]{1,2})\-([0-9]{4})$/', $array_search['from'], $m)) {
-    $array_search['from'] = mktime(0, 0, 0, intval($m[2]), intval($m[1]), intval($m[3]));
-} else {
-    $array_search['from'] = '';
-}
-if (preg_match('/^([0-9]{1,2})\-([0-9]{1,2})\-([0-9]{4})$/', $array_search['to'], $m)) {
-    $array_search['to'] = mktime(23, 59, 59, intval($m[2]), intval($m[1]), intval($m[3]));
-} else {
-    $array_search['to'] = '';
-}
-
-$db->sqlreset()->select('COUNT(*)')->from(NV_PREFIXLANG . '_' . $module_data . '_players');
-
-$where = [];
-if (!empty($array_search['q'])) {
-    $base_url .= '&amp;q=' . urlencode($array_search['q']);
-    $dblikekey = $db->dblikeescape($array_search['q']);
-    $where[] = "(
-        fullname LIKE '%" . $dblikekey . "%' OR
-        keywords LIKE '%" . $dblikekey . "%' OR
-        address LIKE '%" . $dblikekey . "%'
-    )";
-}
-if (!empty($array_search['from'])) {
-    $base_url .= '&amp;f=' . nv_date('d-m-Y', $array_search['from']);
-    $where[] = "time_add>=" . $array_search['from'];
-}
-if (!empty($array_search['to'])) {
-    $base_url .= '&amp;t=' . nv_date('d-m-Y', $array_search['to']);
-    $where[] = "time_add<=" . $array_search['to'];
 }
 
 $xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
@@ -119,8 +118,6 @@ $array_search['from'] = empty($array_search['from']) ? '' : nv_date('d-m-Y', $ar
 $array_search['to'] = empty($array_search['to']) ? '' : nv_date('d-m-Y', $array_search['to']);
 
 $xtpl->assign('SEARCH', $array_search);
-
-
 
 // Hiển thị dữ liệu
 if (!empty($array)) {
