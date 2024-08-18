c767c6c5df9a79cc07766b2174d060929eb524f5
Vi Huynh <hoangvicntt2k@gmail.com>
2024-08-18 16:48:20 +0700

    Thêm chức năng xem danh sách bình chọn (Chưa xong)

---
+++ modules/missworld/admin/voters.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @Project Module Missworld
+ * @Author HUYNH HOANG VI (hoangvicntt2k@gmail.com)
+ * @Copyright (C) 2024 HUYNH HOANG VI. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate March 01, 2024, 08:00:00 AM
+ */
+
+if (!defined('NV_IS_FILE_ADMIN')) {
+    exit('Stop!!!');
+}
